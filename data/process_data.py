9fd87b326da029f8116e6776adfdbab1fa6ea89d
Shaswat Rajput <shaswatrajput@outlook.com>
2020-06-17 13:01:54 -0500

    completed pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -14,3 +14,31 @@ def load_data(messages_filepath, categories_filepath):
     
     return df
 
+def clean_data(df):
+    """Clean the merged dataframe to make it ready to analyze"""
+    
+    # Create column based on values in categories column
+    categories = df['categories'].str.split(';', expand=True)
+
+    # Rename the columns with the proper name
+    row = categories.loc[0,:]
+    category_colnames = row.apply(lambda x: x.split('-')[0]).tolist()
+    categories.columns = category_colnames
+
+    # Clean the value in categories
+    for column in categories:
+
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x.split('-')[1])
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    categories['related'] = categories['related'].replace(2, 1)
+
+    # Replace the original categories column with the new one and drop duplicates
+    df.drop(columns=['categories'], inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace=True)
+
+    return df
