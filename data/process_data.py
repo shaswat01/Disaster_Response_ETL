7242d7eea4f5ecb6db94701fa2b73b9ca03d2639
Shaswat Rajput <shaswatrajput@outlook.com>
2020-06-17 13:45:16 -0500

    removed db error

--- data/process_data.py
+++ data/process_data.py
@@ -42,3 +42,40 @@ def clean_data(df):
     df.drop_duplicates(inplace=True)
 
     return df
+
+def save_data(df, database_filename):
+    """Take the input dataframe and save it into sqlite database"""
+    
+    # Create sqlite engine and save the dataframe with the name messages
+    engine = create_engine('sqlite:///data/{}'.format(database_filename))
+    df.to_sql('messages', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
